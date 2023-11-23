b07cb5bb7b7e03c4501e1290001d61332b47e640
Charlotte <117946642+Charlottecrn@users.noreply.github.com>
2023-11-23 14:05:20 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,7 +39,7 @@ try:
     streamlit.dataframe(fruityvice_normalized)
 except URLError as e:
   streamlit.error()
-
+'''
 streamlit.stop()
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
@@ -52,3 +52,4 @@ add_my_fruit = streamlit.text_input('What fruit would you like to add?','Jackfru
 streamlit.write('Thanks for adding', add_my_fruit)
 
 my_cur.execute("insert into fruit_load_list values ('from streamlit')")
+'''
